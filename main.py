56b2db90506bcaaf2e0c5aedd6316875e1265edb
sarathysrt <sarathysrt@users.noreply.github.com>
2018-06-02 18:01:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def postJsonHandler():
     data.columns=['Time','Record']
     yy=min(data.Record)
     yy = yy*-1 if np.sign(yy) == -1 else yy
-    data.Record=[x*-1 for x in pd.to_numeric(data.Record)]
+    data.Record=[x+yy for x in pd.to_numeric(data.Record)]
     (baseline, ecg_out) = bwr.bwr(data.Record)
     data.Record=ecg_out
     ss=data.to_json(orient='records')
